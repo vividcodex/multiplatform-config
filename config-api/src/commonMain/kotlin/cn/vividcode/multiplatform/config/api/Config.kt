7b76a92cb646ea0a2c016ed99039931fc66958cb
李佳伟 <2965502228@qq.com>
2024-06-09 02:20:58 +0800

    配置信息更加安全的config基本信息配置

--- config-api/src/commonMain/kotlin/cn/vividcode/multiplatform/config/api/Config.kt
+++ config-api/src/commonMain/kotlin/cn/vividcode/multiplatform/config/api/Config.kt
@@ -1,32 +1,64 @@
 package cn.vividcode.multiplatform.config.api
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+
 /**
  * config info
  */
-object Config {
+sealed interface Config {
+	
+	companion object {
+		
+		val aesKey by lazy { ConfigImpl.aesKey }
+		
+		val aesIv by lazy { ConfigImpl.aesIv }
+		
+		val appName by lazy { ConfigImpl.appName }
+		
+		val configFileName by lazy { ConfigImpl.configFileName }
+	}
 	
 	/**
 	 * AES KEY
 	 */
-	var aesKey = "A6j3akipAQaiYeIj"
+	var aesKey: String
 	
 	/**
 	 * AES Iv
 	 */
-	var aesIv = "3pUjGpdtTKXMTysmE2UHzwjnRoXzXlO1esLrvdL5BmKk0J1q9lThStZp4HAX9QUn"
+	var aesIv: String
 	
 	/**
 	 * app name
 	 */
-	var appName = ""
+	var appName: String
 	
 	/**
-	 * name
+	 * config file name
 	 */
-	var name = "config"
+	var configFileName: String
+}
+
+private data object ConfigImpl : Config {
+	
+	override var aesKey = "A6j3akipAQaiYeIj"
+	
+	override var aesIv = "3pUjGpdtTKXMTysmE2UHzwjnRoXzXlO1esLrvdL5BmKk0J1q9lThStZp4HAX9QUn"
+	
+	override var appName = ""
+	
+	override var configFileName = "config"
 }
 
 /**
  * config
  */
 expect inline fun <reified T : Comparable<*>> config(default: T? = null): ConfigProperty<T>
+
+@Composable
+fun config(config: Config.() -> Unit) {
+	LaunchedEffect(Unit) {
+		ConfigImpl.apply(config)
+	}
+}

65f08ad5719b998835da247a723973e7f214d9d9
李佳伟 <2965502228@qq.com>
2024-06-09 02:23:52 +0800

    删除注释信息

--- sample/src/androidMain/kotlin/cn/vividcode/multiplatform/config/sample/MainApplication.kt
+++ sample/src/androidMain/kotlin/cn/vividcode/multiplatform/config/sample/MainApplication.kt
@@ -4,13 +4,7 @@ import android.app.Application
 import cn.vividcode.multiplatform.config.api.ContextConfig
 
 /**
- * 项目：vividcode-multiplatform-config
- *
- * 作者：li-jia-wei
- *
- * 创建：2024/6/9 上午2:13
- *
- * 介绍：MainApplication
+ * MainApplication
  */
 class MainApplication : Application() {
 	
